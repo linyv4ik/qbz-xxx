8c10ec31ce264fcfd31fa6b0416d9a9990b3a6da
linyv4ik <134552208+linyv4ik@users.noreply.github.com>
2023-05-26 00:40:08 +0300

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,13 @@
+@echo off
+chcp 65001
+cls
+python.exe -m pip install --upgrade pip
+pip install pathvalidate
+pip install requests
+pip install mutagen
+pip install tqdm
+pip install pick
+pip install beautifulsoup4
+pip install colorama
+echo Інсталяція завершена
+pause
