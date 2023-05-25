b0365ad4b93506a720844b67b89583dc918311d4
linyv4ik <134552208+linyv4ik@users.noreply.github.com>
2023-05-25 13:05:30 +0300

    Add files via upload

---
+++ install requirements.bat
@@ -0,0 +1,8 @@
+python.exe -m pip install --upgrade pip
+pip install pathvalidate
+pip install requests
+pip install mutagen
+pip install tqdm
+pip install pick
+pip install beautifulsoup4
+pip install colorama
