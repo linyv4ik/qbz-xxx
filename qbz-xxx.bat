df03619a8bb3268f26515856b59bb388f206fc79
linyv4ik <134552208+linyv4ik@users.noreply.github.com>
2024-02-11 20:01:05 +0200

    Update qbz-xxx.bat

--- qbz-xxx.bat
+++ qbz-xxx.bat
@@ -6,6 +6,7 @@ if not exist "qbzhash.py" goto upd
 if not exist "qbzremake.py" goto upd
 if not exist "%cd%\.setting\qobuz-dl\" mkdir "%cd%\.setting\qobuz-dl\"
 ::update checker
+if exist "%tmp%\update.bat" del /f "%tmp%\update.bat"
 if exist "%tmp%\q_version.txt" del /f "%tmp%\q_version.txt"
 set version=1.4
 curl -s "https://raw.githubusercontent.com/linyv4ik/update/main/q_version.txt" --output "%tmp%\q_version.txt"
@@ -368,14 +369,17 @@ if %update% == 1 (goto upd)
 goto choice
 
 :upd
-if exist "%tmp%\qbz-xxx" @rd /s /q "%tmp%\qbz-xxx"
-if exist "%tmp%\qbz-xxx.zip" del /f "%tmp%\qbz-xxx.zip"
-if not exist "%tmp%\qbz-xxx" mkdir "%tmp%\qbz-xxx"
-curl -s "https://codeload.github.com/linyv4ik/qbz-xxx/zip/refs/heads/main" --output "%tmp%\qbz-xxx.zip"
-powershell Expand-Archive "%tmp%\qbz-xxx.zip" -DestinationPath "%tmp%\qbz-xxx" -Force
-xcopy "%tmp%\qbz-xxx\qbz-xxx-main\*" "%cd%" /E /I /Y
-if exist "%tmp%\qbz-xxx" @rd /s /q "%tmp%\qbz-xxx"
-if exist "%tmp%\qbz-xxx.zip" del /f "%tmp%\qbz-xxx.zip"
-timeout /t 2
-start cmd /c "qbz-xxx.bat"
+echo timeout /t 2 >>update.bat
+echo if exist "%tmp%\qbz-xxx" rmdir /s /q "%tmp%\qbz-xxx">>"%tmp%\update.bat"
+echo if exist "%tmp%\qbz-xxx.zip" del /f "%tmp%\qbz-xxx.zip">>"%tmp%\update.bat"
+echo if not exist "%tmp%\qbz-xxx" mkdir "%tmp%\qbz-xxx">>"%tmp%\update.bat"
+echo curl -s "https://codeload.github.com/linyv4ik/qbz-xxx/zip/refs/heads/main" --output "%tmp%\qbz-xxx.zip">>"%tmp%\update.bat"
+echo powershell Expand-Archive "%tmp%\qbz-xxx.zip" -DestinationPath "%tmp%\qbz-xxx" -Force>>"%tmp%\update.bat"
+echo xcopy "%tmp%\qbz-xxx\qbz-xxx-main\*" "%cd%" /E /I /Y>>"%tmp%\update.bat"
+echo if exist "%tmp%\qbz-xxx" rmdir /s /q "%tmp%\qbz-xxx">>"%tmp%\update.bat"
+echo if exist "%tmp%\qbz-xxx.zip" del /f "%tmp%\qbz-xxx.zip">>"%tmp%\update.bat"
+timeout /t 1 >>"%tmp%\update.bat"
+echo start cmd /c "qbz-xxx.bat">>"%tmp%\update.bat"
+timeout /t 1
+start cmd /c "update.bat"
 exit
